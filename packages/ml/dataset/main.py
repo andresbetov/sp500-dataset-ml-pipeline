291a0c860d9f212f4a395efcde3a5ed59487f605
Andrés Bermúdez <91708539+andresbetov@users.noreply.github.com>
2026-04-15 14:35:32 -0500

    feat: Refactor get_dataframe to accept file name and integrate data preparation

--- packages/ml/dataset/main.py
+++ packages/ml/dataset/main.py
@@ -2,10 +2,14 @@ from __future__ import annotations
 
 import pandas as pd
 
-from loader import load_dataframe
+from .loader import load_dataframe
+from .preparation import prepare_dataframe
 
 
-def get_dataframe() -> pd.DataFrame:
-    return load_dataframe()
+def get_dataframe(file_name: str | None = None) -> pd.DataFrame:
+    loaded = load_dataframe(file_name=file_name)
+    return prepare_dataframe(loaded)
 
-print(get_dataframe().head())
+
+if __name__ == "__main__":
+    print(get_dataframe().head())
