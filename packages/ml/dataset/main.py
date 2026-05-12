3819c0435c11830640546168edad2713a882d416
Andrés Bermúdez <91708539+andresbetov@users.noreply.github.com>
2026-05-12 11:34:05 -0500

    feat: rename and simplify file saving methods for parquet format

--- packages/ml/dataset/main.py
+++ packages/ml/dataset/main.py
@@ -12,18 +12,11 @@ SCRIPT_DIR = Path(__file__).resolve().parent
 PROJECT_ROOT = SCRIPT_DIR.parent.parent.parent
 DEFAULT_FILE_DIRECTORY = PROJECT_ROOT / "data" / "processed"
 
-def _save_file(df: pd.DataFrame, file_name: str, file_type: str) -> None:
-    file_path = Path(DEFAULT_FILE_DIRECTORY / (file_name + (".parquet" if file_type == "parquet" else ".csv")))
-    file_path.parent.mkdir(parents=True, exist_ok=True)
-    if file_type == "parquet":
-        df.to_parquet(file_path, index=False)
-    elif file_type == "csv":
-        df.to_csv(file_path, index=False)
 
-# def _save_parquet(df: pd.DataFrame, file_name: str) -> None:
-#     file_path = Path(DEFAULT_FILE_DIRECTORY / (file_name + ".parquet"))
-#     file_path.parent.mkdir(parents=True, exist_ok=True)
-#     df.to_parquet(file_path, index=False)
+def _save_parquet(df: pd.DataFrame, file_name: str) -> None:
+    file_path = Path(DEFAULT_FILE_DIRECTORY / (file_name + ".parquet"))
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+    df.to_parquet(file_path, index=False)
 
 
 def get_dataframe(
