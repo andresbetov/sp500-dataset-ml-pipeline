38bebe8dd6e6033ff908c26ba0663fb0df5e9fb1
Andrés Bermúdez <91708539+andresbetov@users.noreply.github.com>
2026-05-12 10:58:41 -0500

    feat: refactor file saving functionality to support both CSV and Parquet formats (provisional)

--- packages/ml/dataset/main.py
+++ packages/ml/dataset/main.py
@@ -8,24 +8,29 @@ from loader import load_dataframe
 from preparation import prepare_dataframe
 from features import build_features_dataframe
 
+SCRIPT_DIR = Path(__file__).resolve().parent
+PROJECT_ROOT = SCRIPT_DIR.parent.parent.parent
+DEFAULT_FILE_DIRECTORY = PROJECT_ROOT / "data" / "processed"
 
-DEFAULT_PARQUET_PATH = Path("data/processed/sp500_features.parquet")
+def _save_file(df: pd.DataFrame, file_name: str, file_type: str) -> None:
+    file_path = Path(DEFAULT_FILE_DIRECTORY / (file_name + (".parquet" if file_type == "parquet" else ".csv")))
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+    if file_type == "parquet":
+        df.to_parquet(file_path, index=False)
+    elif file_type == "csv":
+        df.to_csv(file_path, index=False)
 
-
-def _save_parquet(df: pd.DataFrame, output_path: str | Path) -> Path:
-    parquet_path = Path(output_path)
-    parquet_path.parent.mkdir(parents=True, exist_ok=True)
-    df.to_parquet(parquet_path, index=False)
-    return parquet_path
+# def _save_parquet(df: pd.DataFrame, file_name: str) -> None:
+#     file_path = Path(DEFAULT_FILE_DIRECTORY / (file_name + ".parquet"))
+#     file_path.parent.mkdir(parents=True, exist_ok=True)
+#     df.to_parquet(file_path, index=False)
 
 
 def get_dataframe(
     file_name: str | None = None,
-    parquet_path: str | Path = DEFAULT_PARQUET_PATH,
 ) -> pd.DataFrame:
     loaded = load_dataframe(file_name=file_name)
     prepared = prepare_dataframe(loaded)
     featured = build_features_dataframe(prepared)
-    _save_parquet(featured, output_path=parquet_path)
+    _save_file(featured, "featured", file_type="csv")
     return featured
-
